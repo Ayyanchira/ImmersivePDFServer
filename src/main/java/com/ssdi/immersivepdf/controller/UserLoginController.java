334dc10df1ff03f8fe8f353ad946ea1840f80967
rakshitneelavar <rakshit.n.rao@gmail.com>
2018-03-23 13:59:15 -0400

    Added controller, dao and model class for login.

---
+++ src/main/java/com/ssdi/immersivepdf/controller/UserLoginController.java
@@ -0,0 +1,29 @@
+package com.ssdi.immersivepdf.controller;
+import com.ssdi.immersivepdf.dao.LoginUserDao;
+import com.ssdi.immersivepdf.model.LoginResponse;
+import com.ssdi.immersivepdf.model.Login;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class UserLoginController {
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public LoginResponse loginUser(@RequestBody Login request) {
+        System.out.println("Registering new user...");
+
+        LoginUserDao loginUserDao = new LoginUserDao();
+        LoginResponse response = new LoginResponse();
+        if (loginUserDao.login(request)) {
+            response.setStatusMessage("Success");
+            response.setStatusCode(200);
+        } else {
+            response.setStatusMessage("Failed");
+            response.setStatusCode(400);
+        }
+        return response;
+    }
+}
---
+++ src/main/java/com/ssdi/immersivepdf/dao/LoginUserDao.java
@@ -0,0 +1,33 @@
+package com.ssdi.immersivepdf.dao;
+
+import com.ssdi.immersivepdf.model.Login;
+import com.ssdi.immersivepdf.util.DBConnector;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class LoginUserDao {
+
+    public boolean login(Login login){
+        try {
+            Connection connection = DBConnector.getConnection();
+            String sql = "SELECT * FROM USER WHERE email=? AND password=?";
+            PreparedStatement pstmt = connection.prepareStatement(sql);
+            pstmt.setString(1, login.getEmail());
+            pstmt.setString(2, login.getPassword());
+            ResultSet res = pstmt.executeQuery();
+            if (res.first()) {
+                return true;
+            }
+            else{
+                return false;
+            }
+            /* connection.close(); */
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
