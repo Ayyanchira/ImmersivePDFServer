aff8b9ed28cef96f74f1587021adf25117fb8474
akshayayyanchira <ayyanchira.akshay@gmail.com>
2018-03-23 16:10:18 -0400

    Removed unneccessary code from DB Util class. Added test class and method to Junit test the getConnection method.

---
+++ src/test/java/com/ssdi/immersivepdf/util/DBConnectorTest.java
@@ -0,0 +1,24 @@
+package com.ssdi.immersivepdf.util;
+
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import static org.junit.Assert.*;
+
+public class DBConnectorTest {
+
+    @Test
+    public void getConnection() {
+        System.out.println("----Testing DB Util -> getConnection()");
+            try {
+                Connection connection = DBConnector.getConnection();
+                assertTrue(connection != null);
+                System.out.println("DB Connection util is working properly");
+            } catch (SQLException e) {
+                e.printStackTrace();
+                System.out.println("DB Connection util failed in creating a connection");
+            }
+    }
+}
