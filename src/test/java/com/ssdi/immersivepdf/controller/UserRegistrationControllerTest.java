bf3cf0c144e98caae7c501cb00bd3ef557df0733
akshayayyanchira <ayyanchira.akshay@gmail.com>
2018-03-27 16:53:56 -0400

    Added test class for register controller. Implementation is pending

---
+++ src/test/java/com/ssdi/immersivepdf/controller/UserRegistrationControllerTest.java
@@ -0,0 +1,13 @@
+package com.ssdi.immersivepdf.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserRegistrationControllerTest {
+
+    @Test
+    public void registerUser() {
+
+    }
+}
