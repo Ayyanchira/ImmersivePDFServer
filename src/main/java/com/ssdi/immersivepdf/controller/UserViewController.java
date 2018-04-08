15f0d95a09e6970059ed98d743bd559599c65bca
akshayayyanchira <ayyanchira.akshay@gmail.com>
2018-04-08 04:32:58 -0400

    Cosmetic changes to view books module. Better error handling in view books service

--- src/main/java/com/ssdi/immersivepdf/controller/UserViewController.java
+++ src/main/java/com/ssdi/immersivepdf/controller/UserViewController.java
@@ -1,34 +1,40 @@
 package com.ssdi.immersivepdf.controller;
 
+import com.ssdi.immersivepdf.dao.RegisterUserDao;
 import com.ssdi.immersivepdf.dao.ViewUserDao;
 import com.ssdi.immersivepdf.model.View.Books;
 import com.ssdi.immersivepdf.model.View.ViewResponse;
 import com.ssdi.immersivepdf.model.View.View;
+import com.ssdi.immersivepdf.model.generic.Response;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.SQLException;
+
 @RestController
 public class UserViewController {
+
+    @Autowired
+    private ViewUserDao viewUserDao;
+
     @RequestMapping(value = "/view", method = RequestMethod.POST)
     //@CrossOrigin(origins = "http://localhost:3000")
-    public Books viewUser(@RequestBody View request) {
-        System.out.println("View PDFs");
+    public Response viewUser(@RequestBody View request) {
 
-        ViewUserDao viewUserDao = new ViewUserDao();
-        ViewResponse response = new ViewResponse();
-
-        Books userBookCollection = viewUserDao.view(request);
-        return userBookCollection;
-//        if (viewUserDao.view(request)) {
-//            response.setStatusMessage("Success");
-//            response.setStatusCode(200);
-//        } else {
-//            response.setStatusMessage("Failed");
-//            response.setStatusCode(400);
-//        }
-//        return response;
+        Response response = new Response();
+        try {
+            Books userBookCollection = viewUserDao.view(request);
+            response.setData(userBookCollection);
+            response.setStatusMessage("Successful");
+            response.setStatusCode(200);
+        }catch (SQLException e){
+            response.setStatusCode(303);
+            response.setStatusMessage("Error while fetching books");
+        }
+        return response;
     }
 }
--- src/main/java/com/ssdi/immersivepdf/dao/ViewUserDao.java
+++ src/main/java/com/ssdi/immersivepdf/dao/ViewUserDao.java
@@ -5,6 +5,7 @@ import com.ssdi.immersivepdf.model.View.Books;
 import com.ssdi.immersivepdf.model.View.View;
 import com.ssdi.immersivepdf.util.ConnectionData;
 import com.ssdi.immersivepdf.util.DBConnector;
+import org.springframework.stereotype.Service;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -12,23 +13,21 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+@Service
 public class ViewUserDao {
 
     private ConnectionData connectionData;
-    public Books view(View view){
+    public Books view(View view) throws SQLException{
         connectionData = new ConnectionData();
         try {
             Connection connection = DBConnector.getConnection(connectionData);
             String sql = "SELECT * FROM BOOKS b JOIN USER u on b.userid = u.userid and email=?";
             PreparedStatement pstmt = connection.prepareStatement(sql);
             pstmt.setString(1, view.getEmail());
-            //pstmt.executeUpdate();
             ResultSet res = pstmt.executeQuery();
 
             ArrayList<Book> userBookCollection = new ArrayList<Book>();
-            //for all the books
             while (res.next()){
-
                 Book book = new Book();
                 book.setBookid(res.getInt("bookid"));
                 book.setBookname(res.getString("bookname"));
@@ -36,27 +35,15 @@ public class ViewUserDao {
                 book.setUserid(res.getInt("userid"));
                 book.setLocation(res.getString("location"));
                 book.setBookmark(res.getInt("bookmark"));
-
                 userBookCollection.add(book);
-
-                //create a book object
-                //store it in a arraylist of type Books
-                // and return Books.
             }
             Books allBooks = new Books();
             allBooks.setBookCollection(userBookCollection);
             return allBooks;
-//            if (res.first()) {
-//                return true;
-//            }
-//            else{
-//                return false;
-//            }
-            /* connection.close(); */
 
         } catch (SQLException e) {
             e.printStackTrace();
-            return new Books();
+            throw e;
         }
     }
 }
